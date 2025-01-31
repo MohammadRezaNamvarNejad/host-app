368e404d6dd3fd1223ae7af37c66c11b16a8d19e
MR.Namvar <mohammadrezanamvar@gmail.com>
2025-01-31 22:11:35 +0330

    Implement module federation with remote component integration and update TypeScript configuration

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,39 @@
-import type { NextConfig } from "next";
+// next.config.js
+const NextFederationPlugin = require("@module-federation/nextjs-mf");
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
+module.exports = {
+  webpack(config, options) {
+    const { isServer } = options;
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: "host",
+        remotes: {
+          // this will contain remote route
+          remoteApp: `remoteApp@http://localhost:3001/_next/static/${
+            isServer ? "ssr" : "chunks"
+          }/remoteEntry.js`,
+        },
+        filename: "static/chunks/remoteEntry.js",
+        exposes: {
+          // host can also expose components
+        },
+        shared: {
+          react: {
+            singleton: true, // this will make sure it will be loaded
+            // only once during runtime
+            eager: true,
+            requiredVersion: false,
+          },
+          "react-dom": {
+            singleton: true,
+            eager: true,
+            requiredVersion: false,
+          },
+          // files or dependencies we want to share
+        },
+      })
+    );
+
+    return config;
+  },
 };
-
-export default nextConfig;
